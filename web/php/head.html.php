9a1ff6539a8c6936e38088d73aae1938e33e3431
Christien Kelly <kelly_c1@denison.edu>
2020-05-23 18:44:06 -0400

    first commit

---
+++ web/php/head.html.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Christien Kelly</title>
+	<link rel="icon" type="image/png" href="icon.png">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat:400,800">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta content="width=device-width,initial-scale=1" name="viewport">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> 
+	<!-- This enables jQuery, this snipet of code uses the online API of JQuery -->
+	<script type="text/javascript">
+		// JQuery lives inside this function.
+		$(document).ready(function(){
+  		// make it so the downwards arrows disappear as you scroll.
+  			$(window).scroll(function() {
+				if ($(document).scrollTop() > 10) {
+    				$('.arrow').slideToggle(1000);}
+  		//Showing and hiding projects, use the Fadetoggle command.
+
+		});
+
+	</script>
+</head>
+<body>
+	<!-- Building the  header navigation Best navigation: https://youtu.be/GxwHXxumdQk-->
+<div class = "header">
+		
+	<div class = "inner_header">
+
+		<div class = "logo_container">
+				<h1><span>Christien</span>Kelly</h1>
+				<!--<img src='logo.png'/>-->	
+		</div>
+
+		<ul class = "navigation">
+			<a href="ChristienKelly–Resume.pdf"><li>Resume</li></a>
+			<a href="about.html"><li>About</li></a>
+			<!-- dash here? -->
+			<a href="https://www.linkedin.com/in/christien-k-770080107/"><li>LinkedIn</li></a>
+			<a href="https://github.com/kelly-c1"><li>Github</li></a>
+		</ul>
+	</div>
+</div>
+</body>
+</html>
