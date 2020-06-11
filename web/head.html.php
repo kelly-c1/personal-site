87e017002cf0481713e33503c86e95c0b57c0e05
Christien Kelly <kelly_c1@denison.edu>
2020-06-11 14:39:36 -0400

    small changes to the header again

--- web/head.html.php
+++ web/head.html.php
@@ -37,9 +37,7 @@
 			<a href="ChristienKelly–Resume.pdf" download><li>Resume</li></a>
 			<!-- <a href="about.html"><li>About</li></a> -->
 			<!-- dash here? -->
-			<a href="https://www.linkedin.com/in/christien-k-770080107/"><li><i class="fa fa-linkedin icon-large"></i></li></a>
-			<a href="https://github.com/kelly-c1"><li><i class="fa fa-github icon-large"></i></li></a>
-			<a href="mailto:kelly_c1@denison.edu"><li><i class="fa fa-envelope icon-large"></i></li></a>
+			<a href="https://www.linkedin.com/in/christien-k-770080107/"><li><i class="fa fa-linkedin icon-large"></i><i class="fa fa-github icon-large"></i><i class="fa fa-envelope icon-large"></i></li></a>
 		</ul>
 	</div>
 </div>
