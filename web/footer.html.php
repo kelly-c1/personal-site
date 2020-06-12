224593687a15e5961a473bfa515ac0c47ee69752
Christien Kelly <kelly_c1@denison.edu>
2020-06-12 11:05:41 -0400

    footer changes

--- web/footer.html.php
+++ web/footer.html.php
@@ -9,9 +9,9 @@
 <body>
 	<div class = "footer">
 		<div class="inner_footer"> 
-			<table class="logo_container"> 
+			<table class="table_container"> 
 				<tr>
-				<td><h1 class="logo_container"><span>Last Updated:</span>June 12, 2020</h1></td>
+				<td><h5 class="footer_date"><span>Last Updated:<br></span>June 12, 2020</h5></td>
 				<td>
 					<ul class = "navigation">
 						<a href="https://twitter.com/ChristienKelly1"><li><i class="fa fa-twitter"></i></li></a>
